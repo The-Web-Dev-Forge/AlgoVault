0cb938c2a57ca219f614ae91fc3f8af0aa652be7
AYUSH MISHRA <connect.ayushommishra@gmail.com>
2025-07-21 22:26:05 +0530

    Delete ChatBot directory

--- ChatBot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'ChatBot'
-
-urlpatterns = [
-    path('chatbot/', views.chatbot_page, name='chatbot'),
-    path('api/chat/', views.chat_api, name='chat_api'),
-]
